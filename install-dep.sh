26db6c4f016cb62fb190b784ee2728bd58f1c4a6
Abdur Rahman Maheer <36255129+JoyGhoshs@users.noreply.github.com>
2025-03-13 13:14:16 +0600

    Fixes Few Dependency Error

---
+++ install-dep.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update && sudo apt-get install -y \
+    build-essential \
+    libpq-dev \
+    curl \
+    libnss3 \
+    libnspr4 \
+    libdbus-1-3 \
+    libcups2 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libxkbcommon0 \
+    libcairo2 \
+    libasound2 \
+    libatspi2.0-0 \
+    libpangocairo-1.0-0 \
+    libpangoft2-1.0-0 \
+    libgdk-pixbuf2.0-0 \
+    libgdk-pixbuf-xlib-2.0-0 \
+    && sudo apt-get clean
+
+pip3 install --upgrade pip
+sudo pip3 install playwright
+playwright install chromium
+
+pip3 install -r requirements.txt
+sudo pip3 install weasyprint
